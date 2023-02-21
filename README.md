93b3a47eca5616fad6245017ccd803a63ec49acc
M Taufiq Permana S <35789663+taufiqpsumarna@users.noreply.github.com>
2023-02-21 13:57:54 +0700

    init

---
+++ README.md
@@ -0,0 +1,46 @@
+## Dashboard Image
+<img src =./images/cadvisor.png width=50%/><br>
+<img src =./images/node_exporter.png width=50%/><br>
+
+This script will install the monitoring kit for grafana dashboard, it will include:
+
+## Resources Monitoring
+- Prometheus
+- Node_Exporter
+- Cadvisor
+
+## Logging Monitoring
+- Loki
+- Promtail
+
+## Grafana Dashboard
+- [Cadvisor Dashboard](https://grafana.com/grafana/dashboards/14282-cadvisor-exporter)
+- [Node Exporter Dashboard](https://grafana.com/grafana/dashboards/1860-node-exporter-full)
+
+### Installation Reference:
+- https://computingforgeeks.com/how-to-install-prometheus-and-node-exporter-on-debian/
+- https://citizix.com/how-to-setup-promtail-grafana-and-loki-for-free-log-management-in-debian-11/
+
+## Server Environment
+This has been tested in machine with spesification below:
+```
+PRETTY_NAME="Ubuntu 22.04 LTS"
+NAME="Ubuntu"
+VERSION_ID="22.04"
+VERSION="22.04 LTS (Jammy Jellyfish)"
+VERSION_CODENAME=jammy
+ID=ubuntu
+ID_LIKE=debian
+HOME_URL="https://www.ubuntu.com/"
+SUPPORT_URL="https://help.ubuntu.com/"
+BUG_REPORT_URL="https://bugs.launchpad.net/ubuntu/"
+PRIVACY_POLICY_URL="https://www.ubuntu.com/legal/terms-and-policies/privacy-policy"
+UBUNTU_CODENAME=jammy
+
+Linux 5.15.0-1028-aws #32-Ubuntu SMP Mon Jan 9 12:28:07 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux
+```
+
+Note: 
+That the monitoring kit maybe will not working properly in another operating system, and it will use the default configuration only, you can customize the configuration before start installing
+
+Credits: M Taufiq Permana Sumarna - February 2023
---
+++ install.sh
@@ -0,0 +1,185 @@
+#!/bin/bash
+
+#Variables for running the script
+BinaryLocation=/usr/local/bin
+PrometheusLibDir=/var/lib/prometheus
+PrometheusConfDir=/etc/prometheus
+LokiConfDir=/etc/loki
+LokiDataDir=/data/loki
+PromtailConfDir=/etc/promtail
+PromtailDataDir=/data/promtail
+LokiVer=v2.7.3
+PromtailVer=v2.7.3
+
+#Prometheus
+echo "Create prometheus user and group"
+sudo groupadd --system prometheus 
+sudo useradd -s /sbin/nologin --system -g prometheus prometheus
+
+echo "Create prometheus configuration and data directory"
+sudo mkdir $PrometheusLibDir
+for i in rules rules.d files_sd; do sudo mkdir -p $PrometheusConfDir${i}; done
+
+echo "Download and install prometheus"
+sudo apt-get update 
+sudo apt-get -y install wget curl unzip zip
+curl -s https://api.github.com/repos/prometheus/prometheus/releases/latest|grep browser_download_url|grep linux-amd64|cut -d '"' -f 4|wget -qi - 
+tar xvf prometheus*.tar.gz 
+cd prometheus*/
+
+echo "Move prometheus binary and tools"
+sudo mv prometheus promtool $BinaryLocation
+sudo mv consoles/ console_libraries/ $PrometheusConfDir
+
+echo "Copy prometheus configuration in $PrometheusConfDir"
+cd ..
+sudo cp ./config/prometheus.yaml $PrometheusConfDir
+rm -rf prometheus
+
+echo "Create prometheus systemd configuration"
+sudo tee /etc/systemd/system/prometheus.service<<EOF
+[Unit]
+Description=Prometheus
+Documentation=https://prometheus.io/docs/introduction/overview/
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+Type=simple
+User=prometheus
+Group=prometheus
+ExecReload=/bin/kill -HUP $MAINPID
+ExecStart=/usr/local/bin/prometheus \
+  --config.file=/etc/prometheus/prometheus.yaml \
+  --storage.tsdb.path=/var/lib/prometheus \
+  --web.console.templates=/etc/prometheus/consoles \
+  --web.console.libraries=/etc/prometheus/console_libraries \
+  --web.listen-address=0.0.0.0:9090 \
+  --web.external-url=
+
+SyslogIdentifier=prometheus
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+echo "Change directory permissions"
+for i in rules rules.d files_sd; do sudo chown -R prometheus:prometheus /etc/prometheus/${i}; done
+for i in rules rules.d files_sd; do sudo chmod -R 775 /etc/prometheus/${i}; done
+sudo chown -R prometheus:prometheus /var/lib/prometheus/
+
+echo "Reload systemd daemon and start prometheus services"
+sudo systemctl daemon-reload
+sudo systemctl start prometheus
+sudo systemctl enable prometheus
+sudo systemctl status prometheus
+
+#Node_Exporters
+echo "Download node_exporter"
+curl -s https://api.github.com/repos/prometheus/node_exporter/releases/latest| grep browser_download_url|grep linux-amd64|cut -d '"' -f 4|wget -qi -
+
+echo "Extract and move binary files"
+tar -xvf node_exporter*.tar.gz
+cd  node_exporter*/
+sudo cp node_exporter $BinaryLocation
+
+echo "Create node_exporter systemd configuration"
+sudo tee /etc/systemd/system/node_exporter.service <<EOF
+[Unit]
+Description=Node Exporter
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=prometheus
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+WantedBy=default.target
+EOF
+
+echo "Check node_exporter version"
+node_exporter --version
+
+echo "Reload systemd daemon and start node_exporter services"
+sudo systemctl daemon-reload
+sudo systemctl start node_exporter
+sudo systemctl enable node_exporter
+systemctl status node_exporter
+
+echo "Restart prometheus services"
+sudo systemctl restart prometheus
+
+#Loki
+echo "Download loki"
+curl -LO https://github.com/grafana/loki/releases/download/${LokiVer}/loki-linux-amd64.zip
+
+echo "Extract and move binary files"
+unzip loki-linux-amd64.zip
+sudo chmod a+x loki-linux-amd64
+sudo mv loki-linux-amd64 $BinaryLocation/loki
+
+echo "Create loki configuration and data directory"
+sudo mkdir $LokiConfDir
+sudo mkdir -p $LokiDataDir
+
+echo "Copy loki configuration"
+sudo cp ./config/loki-config.yaml $LokiConfDir
+
+echo "Create loki systemd configuration"
+sudo tee /etc/systemd/system/loki.service<<EOF
+[Unit]
+Description=Loki service
+After=network.target
+
+[Service]
+Type=simple
+User=root
+
+ExecStart=/usr/local/bin/loki -config.file /etc/loki/loki-config.yaml
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+echo "Reload systemd daemon and start loki services"
+sudo systemctl daemon-reload
+sudo systemctl start loki
+sudo systemctl status loki
+
+#Promtail
+echo "Download promtail"
+curl -LO https://github.com/grafana/loki/releases/download/${LokiVer}/promtail-linux-amd64.zip
+
+echo "Extract and move binary files"
+unzip promtail-linux-amd64.zip
+sudo chmod a+x promtail-linux-amd64
+sudo mv promtail-linux-amd64 $BinaryLocation/promtail
+
+echo "Create promtail configuration and data directory"
+sudo mkdir $PromtailConfDir
+sudo mkdir -p $PromtailDataDir
+
+echo "Copy promtail configuration"
+sudo cp ./config/promtail-config.yaml $PromtailConfDir
+
+echo "Create promtail systemd configuration"
+sudo tee /etc/systemd/system/promtail.service<<EOF
+[Unit]
+Description=Promtail service
+After=network.target
+
+[Service]
+Type=simple
+User=root
+ExecStart=/usr/local/bin/promtail -config.file /etc/promtail/promtail-config.yaml
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+echo "Reload systemd daemon and start loki services"
+sudo systemctl daemon-reload
+sudo systemctl start promtail
+sudo systemctl status promtail
